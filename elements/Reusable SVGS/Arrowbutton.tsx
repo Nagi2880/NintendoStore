c39a7602e6ed2dacd8dd92f37ec1d357134e77f2
Nagi2880 <alexanderamaral17@gmail.com>
2025-02-05 22:56:00 -0300

    Start changing the SVG archicteture

---
+++ elements/Reusable SVGS/Arrowbutton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width={12} role='presentation' data-testid="ChevronDownIcon" color='currentColor'>
+                <path clipRule="evenood" fill='currentColor' d='M27.7 5.3L32 9.8 16 26.7 0 9.8l4.3-4.5L16 17.7z'></path>
+            </svg>
+        </div>
+    )
+}
