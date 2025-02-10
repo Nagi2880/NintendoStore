cb7caff26bc9ef8edf12e5d9c715380b39872495
Nagi2880 <alexanderamaral17@gmail.com>
2025-02-10 04:51:09 -0300

    button sidebar updated

--- elements/SideBarButton.tsx
+++ elements/SideBarButton.tsx
@@ -1,27 +1,41 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import SideBarElement from './SideBarElement'
 import SVG from './SVG'
 import sidebarData from '@/Data/HeaderButtonsDataTwo.json'
 import Arrowbutton from './Reusable SVGS/Arrowbutton'
+
 const SideBarButton = () => {
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const toggleLista = (index : number) => {
+
+        setActiveIndex(activeIndex === index ? null : index);
+};
+
+
     return (
         <div>
-            {sidebarData.map((item, index)=>(
+            {sidebarData.map((item, index: number) => (
                 <div key={index}>
                     <div>
-                    {item.IsSVG ? <SVG
+                    {item.IsSVG ? (
+                    <SVG
                     Pathd={item.SVGPathd}
                     Viewbox={item.SVGviewbox}
                     Width={item.SVGWidth}
                     Role={item.SVGRole}
                     TestID={item.SVGTestID}
-
-                    />: null}
-                        <span>
+                    />
+                    ) : null}
+                    
+                        <button onClick={() => toggleLista(index)} className='SidebarButton-container'>
                             {item.ButtonTitle}
                             <Arrowbutton />
-                        </span>
+                        </button>
                     </div>
+                    {activeIndex === index && (
+
                     <ul>
                         {item.Sidebarlist.map((element, elementIndex) =>(
                             <SideBarElement
@@ -30,16 +44,21 @@ const SideBarButton = () => {
                             ElementURL={element.link}
                             Image={element.URLImage}
                             IsSVG={element.IsSVG}
-                            SVGComponent={element.IsSVG ? <SVG
+                            SVGComponent={
+                                element.IsSVG && (
+                                <SVG
                                 Pathd={element.SVGPathd}
                                 Viewbox={element.SVGviewbox}
                                 Width={element.SVGWidth}
                                 Role={element.SVGRole}
                                 TestID={item.SVGTestID}
-                                />: null}
+                                />
+                                )
+                            }
                             />
                         ))}
                     </ul>
+                    )}
                 </div>
             ))}
         </div>
